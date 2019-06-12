e4ec6fd652220c3a8ac953be89ce995fd38c6312
Finn <finnterdal@gmail.com>
2019-06-12 12:00:43 -0500

    adds starter data in script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
-console.log('the script is running!')
+// This array of exercises mirrors the exercise divs we see in index.html.
+// This might come in handy when we want to make changes to the DOM.
+const exercises = [
+  { id: 1, name: 'Bicycling', completed: false, duration: 30 },
+  { id: 2, name: 'Running', completed: true, duration: 45 },
+  { id: 3, name: 'Swimming', completed: true, duration: 30 },
+  { id: 4, name: 'Running', completed: true, duration: 20 },
+]
